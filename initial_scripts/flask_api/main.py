e8b577f782467792724d928bfa3bec5b3f0918d3
Mark Bruckert <markbruckert1@Gmail.com>
2024-09-28 14:55:24 -0400

    Image to prompt

--- initial_scripts/flask_api/main.py
+++ initial_scripts/flask_api/main.py
@@ -20,6 +20,7 @@ storage_client = storage.Client.from_service_account_json(
 
 FAL_KEY = os.getenv('FAL_KEY')
 
+
 def add_to_firestore(original_url, depth_url, point_cloud_url=''):
     doc_ref = db.collection('images').add({
         'original': original_url,
@@ -28,20 +29,22 @@ def add_to_firestore(original_url, depth_url, point_cloud_url=''):
     })
     return doc_ref
 
+
 def upload_to_gcs(file):
     # Initialize a storage client
     bucket_name = "diffusionearth-images"
     bucket = storage_client.bucket(bucket_name)
-    
+
     # Create a new filename to the file with the current timestamp
     filename = file.filename
-    
+
     # Create a new blob and upload the file's content
     blob = bucket.blob(filename)
     blob.upload_from_file(file)
-    
+
     # Return the public url
     return blob.public_url
+
 
 @app.route('/image-to-image', methods=['POST'])
 @cross_origin()
@@ -51,17 +54,35 @@ def image_to_image():
     file = request.files['file']
     if file.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
-    
+
     # Upload image to gcs server
     gcp_image_url = upload_to_gcs(file)
     print(gcp_image_url)
-    
+
     depth_map_url = marigold_depth_estimation(gcp_image_url)
     # Save to Firestore
     doc_ref = add_to_firestore(gcp_image_url, depth_map_url)
     print(doc_ref)
-    
+
     return jsonify({'depth_map_url': depth_map_url})
+
+
+@app.route('/image-to-prompt', methods=['POST'])
+@cross_origin()
+def image_to_prompt():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+
+    # Upload image to gcs server
+    gcp_image_url = upload_to_gcs(file)
+    print(gcp_image_url)
+
+    prompt = generate_prompt_from_image(gcp_image_url)
+    return jsonify({'prompt': prompt})
+
 
 """@app.route('/prompt-to-image', methods=['POST'])
 @cross_origin()
@@ -96,6 +117,7 @@ def address_to_image():
     # For now, just return the address string
     return jsonify({'address': address})"""
 
+
 def marigold_depth_estimation(image_url):
     handler = fal_client.submit(
         "fal-ai/imageutils/marigold-depth",
@@ -108,6 +130,7 @@ def marigold_depth_estimation(image_url):
         return result['image']['url']
     return None
 
+
 def generate_image_from_prompt(prompt):
     handler = fal_client.submit(
         "fal-ai/fast-sdxl/text-to-image",
@@ -119,6 +142,23 @@ def generate_image_from_prompt(prompt):
     if 'images' in result and len(result['images']) > 0:
         return result['images'][0]['url']
     return None
+
+
+def generate_prompt_from_image(prompt):
+    handler = fal_client.submit(
+        "fal-ai/moondream/batched",
+        arguments={
+            "inputs": [{
+                "prompt": "Describe in detail the contents of this image.",
+                "image_url": prompt
+            }]
+        }
+    )
+    result = handler.get()
+    if 'outputs' in result:
+        return result['outputs'][0]
+    return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
